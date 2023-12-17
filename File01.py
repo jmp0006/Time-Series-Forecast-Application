01755151daf523fc5291415f2ff9f0c195dc5e87
Muskaan Mehra <muskaanmehra2307@gmail.com>
2023-12-17 22:23:14 +0530

    Updated Application Code

--- File01.py
+++ File01.py
@@ -7,102 +7,107 @@ from prophet.plot import add_changepoints_to_plot
 from prophet.diagnostics import cross_validation
 from prophet.diagnostics import performance_metrics
 from prophet.plot import plot_cross_validation_metric
-import json
 from prophet.serialize import model_to_json, model_from_json
 import holidays
 
 import altair as alt
 import plotly as plt
 import plotly.offline as pyoff
+import plotly.express as px
 import plotly.graph_objs as go
 import plotly.figure_factory as ff
-import base64
+
 import itertools
 from datetime import datetime
-import json
-
-def load_csv():    
-    df_input = pd.DataFrame()  
-    df_input=pd.read_csv(input,sep=None ,engine='python', encoding='utf-8',
+from PIL import Image
+    
+def load_csv():  
+    df_input = pd.DataFrame()    
+    df_input=pd.read_csv(input,sep=None, engine='python', encoding='utf-8',
                             parse_dates=True,
                             infer_datetime_format=True)
     return df_input
 
-def prep_data(df):
+def prep_data(df, date_col, metric_col):
     df_input = df.rename({date_col:"ds",metric_col:"y"},errors='raise',axis=1)
-    st.markdown("The selected date column is now labeled as **ds** and the values columns as **y**")
+    st.success("The selected date column is now labeled as **ds** and the Target column as **y**")
     df_input = df_input[['ds','y']]
     df_input =  df_input.sort_values(by='ds',ascending=True)
     return df_input
 
-st.title('Time-Series Forecast using Prophet')
+# TITLE & DESCRIPTION
+st.title('Time-Series Forecasting with Prophet')
 st.write('This app enables you to generate time series forecast.')
+
+with st.sidebar:
+    st.image(Image.open("Streamlit_image.png"), use_column_width=True)
+    st.header('_**Streamlit Prophet**_')
+    st.subheader('A web-app to train, evaluate and optimise forecasting model in a visual way using **[Prophet](https://facebook.github.io/prophet/)**.')
 
 df =  pd.DataFrame()   
 
-st.subheader('1. Data loading')
-st.write("Import a time series csv file.")
-with st.expander("Data format"): 
-    st.write("The dataset can contain multiple columns but you will need to select a column to be used as dates and a second column containing the metric you wish to forecast. The columns will be renamed as **ds** and **y** to be compliant with Prophet. Even though we are using the default Pandas date parser, the ds (datestamp) column should be of a format expected by Pandas, ideally YYYY-MM-DD for a date or YYYY-MM-DD HH:MM:SS for a timestamp. The y column must be numeric.")
+#SELECTING DATASET-- DATA LOADING
+st.subheader('Data loading')
 
-input = st.file_uploader('')
+#UPLOADING DATASET
+input = st.file_uploader('Upload a Dataset')
 
-if input:
+if input is not None:
     with st.spinner('Loading data..'):
         df = load_csv()
+
+if not df.empty:
         columns = list(df.columns)
         
         col1,col2 = st.columns(2)
         with col1:
-            date_col = st.selectbox("Select date column",index= 0,options=columns,key="date")
+            date_col = st.selectbox("Select date column",index= 0,options=columns,key="date", help='Column to be parsed as a date')
         with col2:
-            metric_col = st.selectbox("Select values column",index=1,options=columns,key="values")
+            metric_col = st.selectbox("Select Target column",index=1,options=columns,key="values", help='Quantity to be forecasted')
 
-        df = prep_data(df)
+        df = prep_data(df, date_col, metric_col)
         output = 0
 
-if st.checkbox('Chart data',key='show'):
-    with st.spinner('Plotting data..'):
-        col1,col2 = st.columns(2)
-    with col1:
-        st.dataframe(df)
-                    
-    with col2:    
-        st.write("Dataframe description:")
-        st.write(df.describe())
-
-    try:
-        line_chart = alt.Chart(df).mark_line().encode(
-            x = 'ds:T',
-            y = "y:Q",tooltip=['ds:T', 'y']).properties(title="Time series preview").interactive()
-        st.altair_chart(line_chart,use_container_width=True)
+        Options = st.radio('Options', ['Plot Time-Seies data', 'Show Dataframe', 'Show Descriptive Statistics'], horizontal=True, label_visibility='collapsed', key = 'options')
+        
+        if Options == 'Plot Time-Seies data':
+            try:
+                line_chart = alt.Chart(df).mark_line().encode(
+                    x = 'ds:T',
+                    y = "y:Q",tooltip=['ds:T', 'y']).properties(title="Time series preview").interactive()
+                st.altair_chart(line_chart,use_container_width=True)
                 
-    except:
-        st.line_chart(df['y'],use_container_width =True,height = 300)
+            except:
+                st.line_chart(df['y'],use_container_width =True,height = 300)
 
+        if Options =='Show Dataframe':
+            st.dataframe(df, use_container_width=True)
+        if Options == 'Show Descriptive Statistics':
+            st.write(df.describe().T, use_container_width=True)
 
-st.subheader("2. Parameters configuration")
+# PARAMETERS CONFIGURATION
+
+st.subheader("Parameters configuration")
 with st.container():
     st.write('In this section you can modify the algorithm settings.')
             
-    with st.expander("Horizon"):
+    with st.expander("**Horizon**"):
         periods_input = st.number_input('Select how many future periods (days) to forecast.',
         min_value = 1, max_value = 366,value=90)
 
-    with st.expander("Seasonality"):
+    with st.expander("**Seasonality**"):
         st.markdown("""The default seasonality used is additive, but the best choice depends on the specific case, therefore specific domain knowledge is required.""")
         seasonality = st.radio(label='Seasonality',options=['additive','multiplicative'])
 
-    with st.expander("Trend components"):
+    with st.expander("**Trend components**"):
         st.write("Add or remove components:")
         daily = st.checkbox("Daily")
         weekly= st.checkbox("Weekly")
         monthly = st.checkbox("Monthly")
         yearly = st.checkbox("Yearly")
 
-    with st.expander("Growth model"):
-        st.write('Prophet uses by default a linear growth model.')
-            
+    with st.expander("**Growth model**"):
+        st.write('Prophet uses by default a linear growth model.')         
 
         growth = st.radio(label='Growth model',options=['linear',"logistic"]) 
 
@@ -122,7 +127,7 @@ with st.container():
             cap = st.slider('Cap',min_value=0.0,max_value=1.0,step=0.05)
             floor = st.slider('Floor',min_value=0.0,max_value=1.0,step=0.05)
             if floor > cap:
-                st.error('Invalid settings. Cap must be higher then floor.')
+                st.error('Invalid settings. Cap must be higher than floor.')
                 growth_settings={}
 
             if floor == cap:
@@ -135,7 +140,7 @@ with st.container():
                 df['cap']=cap
                 df['floor']=floor
 
-    with st.expander('Holidays'):    
+    with st.expander('**Holidays**'):    
         countries = ['Country name','United States','India', 'United Kingdom', 'France','Germany']
         with st.container():
             years=[2023]
@@ -166,7 +171,7 @@ with st.container():
                             
             holidays = st.checkbox('Add country holidays to the model')
 
-    with st.expander('Hyperparameters'):
+    with st.expander('**Hyperparameters**'):
         st.write('In this section it is possible to tune the scaling coefficients.')
             
         seasonality_scale_values= [0.1, 1.0,5.0,10.0]    
@@ -178,8 +183,9 @@ with st.container():
         st.write("The seasonality change point controls the flexibility of the seasonality.")
         seasonality_scale= st.select_slider(label= 'Seasonality prior scale',options=seasonality_scale_values)
 
+#FORECAST
 with st.container():
-    st.subheader("3. Forecast 🔮")
+    st.subheader("Forecast")
     st.write("Fit the model on the data and generate future prediction.")
 
     if input:            
@@ -196,7 +202,7 @@ with st.container():
                     m.add_country_holidays(country_name=selected_country)
                         
                 if monthly:
-                        m.add_seasonality(name='monthly', period=30.4375, fourier_order=5)
+                    m.add_seasonality(name='monthly', period=30.5, fourier_order=5)
 
                 with st.spinner('Fitting the model..'):
                     m = m.fit(df)
@@ -208,22 +214,24 @@ with st.container():
 
             else:
                 st.warning('Invalid configuration')
-
+           
         if st.checkbox("Generate forecast (Predict)",key="predict"):
             try:
                 with st.spinner("Forecasting.."):
                     forecast = m.predict(future)
                     st.success('Prediction generated successfully')
-                    st.dataframe(forecast)
+                    st.dataframe(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']], use_container_width=True)
                     fig1 = m.plot(forecast)
-                    st.write(fig1)
+                    st.write('Forecast plot')
+                    st.write(fig1)                   
+                    #st.pyplot(fig1, use_container_width=True)
                     output = 1
 
-                    if growth == 'linear':
-                        fig2 = m.plot(forecast)
-                        a = add_changepoints_to_plot(fig2.gca(), m, forecast)
-                        st.write(fig2)
-                        output = 1
+                if growth == 'linear':
+                    fig2 = m.plot(forecast)
+                    a = add_changepoints_to_plot(fig2.gca(), m, forecast)
+                    st.write(fig2)
+                    output = 1
             except:
                 st.warning("You need to train the model first.. ")
 
@@ -234,74 +242,60 @@ with st.container():
                     st.write(fig3)
             except: 
                 st.warning("Requires forecast generation..")
-
-    st.subheader('4. Model validation 🧪')
+   
+# CROSS-VALIDATION    
+    st.subheader('Model validation')
     st.write("In this section it is possible to do cross-validation of the model.")
-    with st.expander("Explanation"):
+    with st.expander("**Explanation**"):
         st.markdown("""The Prophet library makes it possible to divide our historical data into training data and testing data for cross validation. The main concepts for cross validation with Prophet are:""")
-        st.write("Training data (initial): The amount of data set aside for training. The parameter is in the API called initial.")
+        st.write("Training data (initial): The amount of data set aside for training.")
         st.write("Horizon: The data set aside for validation.")
-        st.write("Cutoff (period): a forecast is made for every observed point between cutoff and cutoff + horizon.""")
+        st.write("Period: a forecast is made for every observed point between cutoff and cutoff + horizon.""")
 
-    with st.expander("Cross validation"):    
-        initial = st.number_input(value= 365,label="initial",min_value=30,max_value=1096)
-        initial = str(initial) + " days"
+    with st.expander("**Cross-validation**"):    
+        initial = st.number_input(value= 180,label="initial",min_value=30)
 
-        period = st.number_input(value= 90,label="period",min_value=1,max_value=365)
-        period = str(period) + " days"
+        period = st.number_input(value= 30,label="period",min_value=30,max_value=365)
 
-        horizon = st.number_input(value= 90, label="horizon",min_value=30,max_value=366)
-        horizon = str(horizon) + " days"
+        horizon = st.number_input(value= 30, label="horizon",min_value=30,max_value=365)
 
-        st.write(f"Here we do cross-validation to assess prediction performance on a horizon of **{horizon}** days, starting with **{initial}** days of training data in the first cutoff and then making predictions every **{period}**.")
-        
-    with st.expander("Metrics"):            
+    with st.expander("**Metrics**"):            
         if input:
             if output == 1:
                 metrics = 0
             if st.checkbox('Calculate metrics'):
                  with st.spinner("Cross validating.."):
                     try:
-                        df_cv = cross_validation(m, initial=initial,
-                                                period=period, 
-                                                horizon = horizon,
-                                                parallel="processes")                                        
-                            
-                            
+                        df_cv = cross_validation(m, initial=int(initial),
+                                                period=int(period), 
+                                                horizon = int(horizon),
+                                                parallel="processes")                                                                  
                             
                         df_p= performance_metrics(df_cv)
-                        st.write(df_p)
+                        # PERFORMANCE METRICS TABLE
+                        st.dataframe(df_p)
                         metrics = 1
 
-                    except:
-                        st.write("Invalid configuration. Try other parameters.")
-                        metrics = 0
-
-                    st.markdown('**Metrics definition**')
-                    st.write("Mse: mean absolute error")
-                    st.write("Mae: Mean average error")
-                    st.write("Mape: Mean average percentage error")
-                    st.write("Mse: mean absolute error")
-                    st.write("Mdape: Median average percentage error")
+                    except Exception as e:
+                        st.error(f"Error during Cross-validation: {e}")
+                        metrics=0
 
                     if metrics == 1:
-                        metrics = ['Choose a metric','mse','rmse','mae','mape','mdape','coverage']
+                        metrics = ['mse','rmse','mae','mape','mdape','coverage']
                         selected_metric = st.selectbox("Select metric to plot",options=metrics)
-                        if selected_metric != metrics[0]:
+                        if selected_metric:
                             fig4 = plot_cross_validation_metric(df_cv, metric=selected_metric)
                             st.write(fig4)
 
         else:
             st.write("Create a forecast to see metrics")
 
-
-    st.subheader('5. Hyperparameter Tuning 🧲')
+# HYPER-PARAMETER TUNING
+    st.subheader('Hyperparameter Tuning')
     st.write("In this section it is possible to find the best combination of hyperparamenters.")
 
-    param_grid = {  
-                            'changepoint_prior_scale': [0.01, 0.1, 0.5, 1.0],
-                            'seasonality_prior_scale': [0.1, 1.0, 5.0, 10.0],
-                        }
+    param_grid = {'changepoint_prior_scale': [0.01, 0.1, 0.5, 1.0],
+                  'seasonality_prior_scale': [0.1, 1.0, 5.0, 10.0],}
 
     # Generate all combinations of parameters
     all_params = [dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())]
@@ -322,61 +316,37 @@ with st.container():
                                                             parallel="processes")
                             df_p = performance_metrics(df_cv, rolling_window=1)
                             rmses.append(df_p['rmse'].values[0])
-                    except:
-                        for params in all_params:
-                            m = Prophet(**params).fit(df)  # Fit model with given params
-                            df_cv = cross_validation(m, initial=initial,
-                                                            period=period,
-                                                            horizon=horizon,
-                                                            parallel="threads")
-                            df_p = performance_metrics(df_cv, rolling_window=1)
-                            rmses.append(df_p['rmse'].values[0])
+                    except Exception as e:
+                        st.error(f"Error during Hyperparameter optimization: {e}")
+                        
 
                 # Find the best parameters
                 tuning_results = pd.DataFrame(all_params)
                 tuning_results['rmse'] = rmses
-                st.write(tuning_results)
+                st.dataframe(tuning_results)
                             
                 best_params = all_params[np.argmin(rmses)]
                     
                 st.write('The best parameter combination is:')
                 st.write(best_params)
-                st.write(" You may repeat the process using these parameters in the configuration section 2")
+                
         else:
             st.write("Create a model to optimize") 
 
-    st.subheader('6. Export results ✨')     
-    st.write("Finally you can export your result forecast, model configuration and evaluation metrics.")
+    st.subheader('Export results')     
+    st.write("Finally you can export your result forecast.")
         
     if input:
-        if output == 1:
-            col1, col2, col3, col4 = st.columns(4)
-
-            with col1:                 
-                if st.button('Export forecast (.csv)'):
-                    with st.spinner("Exporting.."):
-                        #export_forecast = pd.DataFrame(forecast[['ds','yhat_lower','yhat','yhat_upper']]).to_csv()
-                        export_forecast = pd.DataFrame(forecast[['ds','yhat_lower','yhat','yhat_upper']])
-                        st.write(export_forecast.head())
-                        export_forecast= export_forecast.to_csv(decimal=',')
-                        b64 = base64.b64encode(export_forecast.encode()).decode()
-                        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (click derecho > guardar como **forecast.csv**)'
-                        st.markdown(href, unsafe_allow_html=True)
-            
-            with col2:    
-                if st.button("Export model metrics (.csv)"):
-                    try:
-                        df_p = df_p.to_csv(decimal=',')
-                        b64 = base64.b64encode(df_p.encode()).decode()
-                        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (click derecho > guardar como **metrics.csv**)'
-                        st.markdown(href, unsafe_allow_html=True)
-                    except:
-                        st.write("No metrics to export")
-
-            with col3:
-                if st.button('Save model configuration (.json) in memory'):
-                    with open('serialized_model.json', 'w') as fout:
-                        json.dump(model_to_json(m), fout)
-
-        else:
-            st.write("Generate a forecast to download.")
+        if output == 1:               
+            forecast_df = pd.DataFrame(forecast[['ds','yhat','yhat_lower','yhat_upper']])
+            forecast_data = forecast_df.to_csv(index=False)
+            st.download_button(label="Download Forecast CSV",
+                                data=forecast_data,
+                                file_name=f'Forecast_results.csv',
+                                mime='text/csv',
+                                key='download forecast')
+            st.dataframe(forecast_df, use_container_width=True)
+             
+         
+                
+   
