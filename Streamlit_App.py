6b15bc5ef2e8c740b3912eccf5af82e08a255d91
Muskaan Mehra <muskaanmehra2307@gmail.com>
2023-12-22 23:15:43 +0530

    Updated code

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -40,12 +40,12 @@ def prep_data(df, date_col, metric_col):
 st.title('Time-Series Forecasting with Prophet')
 st.write('This app enables you to generate time series forecast.')
 
-# with st.sidebar:
-#     #image_url = "https://raw.githubusercontent.com/MuskaanMehra/Time-Series-Forecast-Application/blob/main/Streamlit_image.png"
-#     #st.image(image_url, use_column_width=True)
-#     st.image(Image.open('Streamlit_image.png'), use_column_width=True)
-#     st.header('_**Streamlit Prophet**_')
-#     st.subheader('A web-app to train, evaluate and optimise forecasting model in a visual way using **[Prophet](https://facebook.github.io/prophet/)**.')
+with st.sidebar:
+    image_url = "https://raw.githubusercontent.com/MuskaanMehra/Time-Series-Forecast-Application/blob/main/Streamlit_image.png"
+    st.image(image_url, use_column_width=True)
+    #st.image(Image.open('Streamlit_image.png'), use_column_width=True)
+    st.header('_**Streamlit Prophet**_')
+    st.subheader('A web-app to train, evaluate and optimise forecasting model in a visual way using **[Prophet](https://facebook.github.io/prophet/)**.')
 
 df =  pd.DataFrame()   
 
